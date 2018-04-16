123bcfb873a151816a7bb7e5f00273b7cb39dff5
Varun <varun18june@gmail.com>
2018-04-16 18:17:09 +0530

    forget pwd email settings done for production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "adminDevise_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'http://pureloan.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'testtalentelgia@gmail.com',
+    password:             'talentelgia',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  config.mailer_sender = 'testtalentelgia@gmail.com'
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
