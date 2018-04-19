a0d070666c1d218a38a01673e52be8610aec9f53
Varun <varun18june@gmail.com>
2018-04-19 12:48:09 +0530

    Q32015 TXT File added

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,12 +2,18 @@ class City < ApplicationRecord
   #validates :city,:state_code,:zip,:latitude,:longitude,presence: true
   #validates :county, presence: true
 
+  # def self.city_in_state(state)
+  #   where("lower(state_code) like ?","#{state.downcase}%").uniq(&:city).sort_by(&:city).pluck(:city).first(50)
+  # end
+
+  # def self.next_cities(state,last_city)
+  #   where("state_code = ? AND city > ?", state, last_city).uniq(&:city).sort_by(&:city).pluck(:city).first(50)
+  # end
+
   def self.city_in_state(state)
-    where("lower(state_code) like ?","#{state.downcase}%").uniq(&:city).sort_by(&:city).pluck(:city).first(50)
+    where("lower(state_code) like ?","#{state.downcase}%").uniq(&:city).sort_by(&:id).first(50)
   end
-
   def self.next_cities(state,last_city)
-    where("state_code = ? AND city > ?", state, last_city).uniq(&:city).sort_by(&:city).pluck(:city).first(50)
+    where("state_code = ? AND id > ?", state, last_city.to_i).uniq(&:city).sort_by(&:id).first(50)
   end
-
 end
