d224d0de656884388691b4c8b80845187e9795fc
Varun <varun18june@gmail.com>
2018-06-08 18:20:53 +0530

    code updated

--- app/models/factual_mortgage_company.rb
+++ app/models/factual_mortgage_company.rb
@@ -2,16 +2,17 @@ class FactualMortgageCompany < ApplicationRecord
   #validates :factual_id,:name,:address,:city,:state,:zip,:country,:latitude,:longitude,:phone, presence: true
 
 
-  def self.loan_companies_for_city(zip)
-    loan_companies =  where(zip: zip*100) 
+  def self.loan_companies_for_city(city_obj)
+    zip = city_obj.zip
+    loan_companies = where(city: city_obj.city, state: city_obj.state_code)
+    loan_companies =  loan_companies.to_a + where(zip: zip)      if loan_companies.count < 10
     unless loan_companies.count > 10
-     loan_companies += where("zip::text like ?", "#{zip.to_s.first(4).to_i}%")   
+     loan_companies = loan_companies.to_a + where("zip::text like ?", "#{zip.to_s.first(4).to_i}%")   
     end
-  
     unless loan_companies.count > 10
-     loan_companies += where("zip::text like ?", "#{zip.to_s.first(3).to_i}%")
+     loan_companies = loan_companies.to_a + where("zip::text like ?", "#{zip.to_s.first(3).to_i}%")
     end
-    return  loan_companies.first(10)
+    return  loan_companies.uniq.first(10)
   end  
 
 end
--- app/models/loan_officer.rb
+++ app/models/loan_officer.rb
@@ -3,20 +3,59 @@ class LoanOfficer < ApplicationRecord
 
   #require 'fuzzy_match'
 
-  def self.loan_officers_list(bank_name, term)    
-    #LoanOfficer.fuzzy_search(company: bank_name, title: term).order("RANDOM()").first(5)
-    LoanOfficer.fuzzy_search(company: bank_name, title: term).first(5)
+  def self.loan_officers_list(bank_name, term, state_code)    
+    #LoanOfficer.fuzzy_search(company: bank_name, title: term).first(5)
+    if term == 'auto'
+      officers =  LoanOfficer.fuzzy_search(company: bank_name, state: state_code )[10..14]
+      officers = LoanOfficer.fuzzy_search(company: bank_name)[10..14]     unless officers.present? 
+    elsif term == 'personal'
+      officers = LoanOfficer.fuzzy_search(company: bank_name, title: term ).first(5)
+        if officers.present? 
+          if officers.count < 5
+            off_data = LoanOfficer.fuzzy_search(company: bank_name, state: state_code)[5..9]
+            if off_data.present?
+              officers = officers +  off_data          
+            else
+              off_data_state = LoanOfficer.fuzzy_search(company: bank_name)[5..9]
+              officers = officers + off_data_state      if off_data_state.present?
+            end  
+          end
+        else 
+          officers = LoanOfficer.fuzzy_search(company: bank_name, state: state_code)[5..9]
+          officers = LoanOfficer.fuzzy_search(company: bank_name)[5..9]    unless officers.present?
+        end
+    else
+      officers = LoanOfficer.fuzzy_search(company: bank_name, title: term ).first(5)
+        if officers.present?
+          if  officers.count < 5 
+            off_data = LoanOfficer.fuzzy_search(company: bank_name, state: state_code).first(5)
+            if  off_data.present? 
+              officers = officers + off_data  
+            else 
+              off_data_state = LoanOfficer.fuzzy_search(company: bank_name).first(5)
+              officers = officers + off_data_state      if off_data_state.present?
+            end 
+          end
+        else
+          officers = LoanOfficer.fuzzy_search(company: bank_name, state: state_code).first(5)
+          officers = LoanOfficer.fuzzy_search(company: bank_name)[0..4]    unless officers.present?
+        end   
+    end
+    return officers.uniq.first(5)     if officers.present?
   end
 
-  def self.loan_officers_for_city(zip) 
-   loan_officers =  where(zip: zip)
+
+  def self.loan_officers_for_city(city_obj) 
+    zip = city_obj.zip 
+    loan_officers = where(city: city_obj.city, state: city_obj.state_code)
+    loan_officers =  loan_officers.to_a + where(zip: zip)      if loan_officers.count < 10
     unless loan_officers.count > 10 
-     loan_officers += where("zip::text like ?", "#{zip.to_s.first(4).to_i}%")
+     loan_officers = loan_officers.to_a + where("zip::text like ?", "#{zip.to_s.first(4).to_i}%")
     end
     unless loan_officers.count > 10
-     loan_officers += where("zip::text like ?", "#{zip.to_s.first(3).to_i}%")
+     loan_officers = loan_officers.to_a + where("zip::text like ?", "#{zip.to_s.first(3).to_i}%")
     end
-   return loan_officers.first(10)
+   return loan_officers.uniq.first(10)
   end
 
 end
