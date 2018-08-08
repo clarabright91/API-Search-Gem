636f40a430916f2e30316bd067eb5938ba0bf013
Varun <varun18june@gmail.com>
2018-08-08 15:53:34 +0530

    aws s3 bucket added

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,12 +4,12 @@ Created:        26-03-2018
 Purpose:        scheduling and executing jobs
 =end
 
-# require 'rufus-scheduler'
-# scheduler = Rufus::Scheduler.new
-#    unless defined?(Rails::Console) || File.split($0).last == 'rake'
-#      scheduler.every '50s' do #calling scheduler after every 50 seconds
-#       nsh = NewsSearchHistory.find_by("updated_at IS NULL OR date(updated_at) <= ?", 3.months.ago.to_date) #Fetching uniq news search history
-#        #nsh = NewsSearchHistory.find_by("updated_at IS NULL OR date(updated_at) <= ?", 10.days.ago.to_date)
-#        NewsWorker.perform_async(nsh.id) if nsh.present? #sending news_search_history id for background worker
-#      end
-#    end
+require 'rufus-scheduler'
+scheduler = Rufus::Scheduler.new
+   unless defined?(Rails::Console) || File.split($0).last == 'rake'
+     scheduler.every '50s' do #calling scheduler after every 50 seconds
+      nsh = NewsSearchHistory.find_by("updated_at IS NULL OR date(updated_at) <= ?", 3.months.ago.to_date) #Fetching uniq news search history
+       #nsh = NewsSearchHistory.find_by("updated_at IS NULL OR date(updated_at) <= ?", 10.days.ago.to_date)
+       NewsWorker.perform_async(nsh.id) if nsh.present? #sending news_search_history id for background worker
+     end
+   end
